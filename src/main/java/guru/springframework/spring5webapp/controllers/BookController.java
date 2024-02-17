0bbbc21009c877c93934d274e3c2199a66b3b572
Nthabeleng <nthabielle@live.com>
2024-02-17 16:20:21 +0200

    Updating Book controller

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -2,6 +2,7 @@ package guru.springframework.spring5webapp.controllers;
 
 import guru.springframework.spring5webapp.model.Book;
 import guru.springframework.spring5webapp.repository.BookRepository;
+import guru.springframework.spring5webapp.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +11,15 @@ import java.util.Optional;
 
 @Controller
 public class BookController {
-    private BookRepository bookRepository;
+    private final BookService bookService;
 
-    public BookController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
     }
 
     @RequestMapping("/books")
     public String getBooks(Model model){
-        model.addAttribute("books", bookRepository.findAll()); //get a list of books from the database
+        model.addAttribute("books", bookService.findAll()); //get a list of books from the database
 
         return "books";
     }
